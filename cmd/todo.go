647bf21b78666e0c7035fe2bad97be3b9f35adf6
totodev999 <tomoya0814tt@gmail.com>
2024-12-31 17:23:12 +0900

    Add method get , put, delete

--- cmd/todo.go
+++ cmd/todo.go
@@ -13,6 +13,23 @@ import (
 	"main.go/models"
 )
 
+func dateConverter(date string) (time.Time, error) {
+	formattedDueData := strings.ReplaceAll(date, "/", "-")
+
+	var dueDateParsed time.Time
+	var err error
+
+	dueDateParsed, err = time.Parse("2006-01-02", formattedDueData)
+	if err != nil {
+		dueDateParsed, err = time.Parse("2006-1-2", formattedDueData)
+		if err != nil {
+			return time.Time{}, err
+		}
+	}
+	return dueDateParsed, nil
+
+}
+
 // todoCmd represents the todo command
 var todoCmd = &cobra.Command{
 	Use:   "todo",
@@ -24,46 +41,104 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		operation, _ := cmd.Flags().GetString("operation")
 		title, err1 := cmd.Flags().GetString("title")
 		description, err2 := cmd.Flags().GetString("description")
 		dueDate, err3 := cmd.Flags().GetString("due_date")
+		id, _ := cmd.Flags().GetInt("id")
+
+		fmt.Println(id)
 
 		if err1 != nil || err2 != nil || err3 != nil {
 			fmt.Println("Error getting flags")
 			return
 		}
 
-		if title == "" || dueDate == "" || description == "" {
-			fmt.Println("Please provide title, description, due date")
-			return
-		}
-
-		// replace slash to hyphen in dueDate
-		formattedDueData := strings.ReplaceAll(dueDate, "/", "-")
-		fmt.Println("formattedDueData: ", formattedDueData)
-		// Convert dueDate to time.Time
-		dueDateParsed, err := time.Parse("2006-01-02", formattedDueData)
-		parsedDueDate := dueDateParsed.Format("2006/01/02")
-
-		if err != nil {
-			fmt.Println("Error parsing due date")
-			return
-		}
-
 		database.Connect()
 		models.Migrate(database.DB)
 
-		todo := models.Todo{Title: title, Description: description, DueDate: parsedDueDate}
-		database.DB.Create(&todo)
+		switch operation {
+		case "post":
+			if title == "" || dueDate == "" || description == "" {
+				fmt.Println("Please provide title, description, due date")
+				return
+			}
 
-		fmt.Println("Todo created successfully!")
+			dueDateParsed, err := dateConverter(dueDate)
+			if err != nil {
+				fmt.Println("Error parsing due date")
+				return
+			}
+			parsedDueDate := dueDateParsed.Format("2006/01/02")
 
+			if err != nil {
+				fmt.Println("Error parsing due date")
+				return
+			}
+
+			todo := models.Todo{Title: title, Description: description, DueDate: parsedDueDate}
+			database.DB.Create(&todo)
+			fmt.Println("Todo created successfully!")
+
+		case "get":
+			var todos []models.Todo
+			if id > 0 {
+				var todo models.Todo
+				database.DB.First(&todo, id)
+				fmt.Printf("ID: %d\nTitle: %s\nDescription: %s\nDue Date: %s\n", todo.ID, todo.Title, todo.Description, todo.DueDate)
+			} else {
+				database.DB.Find(&todos)
+				for _, todo := range todos {
+					fmt.Printf("ID: %d\nTitle: %s\nDescription: %s\nDue Date: %s\n\n", todo.ID, todo.Title, todo.Description, todo.DueDate)
+				}
+			}
+
+		case "put":
+			if id == 0 {
+				fmt.Println("Please provide todo ID")
+				return
+			}
+			var todo models.Todo
+			database.DB.First(&todo, id)
+
+			if title != "" {
+				todo.Title = title
+			}
+			if description != "" {
+				todo.Description = description
+			}
+			if dueDate != "" {
+				dueDateParsed, err := dateConverter(dueDate)
+				if err != nil {
+					fmt.Println("Error parsing due date")
+					return
+				}
+				todo.DueDate = dueDateParsed.Format("2006/01/02")
+			}
+
+			database.DB.Save(&todo)
+			fmt.Println("Todo updated successfully!")
+
+		case "delete":
+			if id == 0 {
+				fmt.Println("Please provide todo ID")
+				return
+			}
+			var todo models.Todo
+			database.DB.Delete(&todo, id)
+			fmt.Println("Todo deleted successfully!")
+
+		default:
+			fmt.Println("Invalid operation. Use post, get, put, or delete")
+		}
 	}}
 
 func init() {
 	rootCmd.AddCommand(todoCmd)
-	todoCmd.Flags().StringP("title", "T", "", "todo title")
-	todoCmd.Flags().StringP("description", "D", "", "description")
-	todoCmd.Flags().StringP("due_date", "d", "", "due_date")
+	todoCmd.Flags().StringP("operation", "O", "", "Operation(get, post, put, delete)")
+	todoCmd.Flags().IntP("id", "I", 0, "Id of each todo. Necessary when put and delete operation")
+	todoCmd.Flags().StringP("title", "T", "", "todo title. Necessary when post operation")
+	todoCmd.Flags().StringP("description", "D", "", "description. Necessary when post operation")
+	todoCmd.Flags().StringP("due_date", "d", "", "due_date. Necessary when post operation")
 
 }
